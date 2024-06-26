9297907b5e1ec0234ed5b12d618058ccef8e7250
Johnson Osei Yeboah <johnyoat@gmail.com>
2024-06-26 13:43:40 -0500

    feat: api docs for schedule endpoint

--- internal/handler/schedlue_handler.go
+++ internal/handler/schedlue_handler.go
@@ -7,6 +7,16 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// CreateSchedule func create schedule in the database by checking with
+// @Description create schedule in the database
+// @Summary create a schedule
+// @Tags Schedules
+// @Accept json
+// @Produce json
+// @Success 200 {object} model.ResponseObject{data=model.Schedule} "desc"
+// @Param id path string true "Schedule ID"
+// @Param request body model.Schedule true "query params"
+// @Router /schedules/{id} [post]
 func CreateSchedule(c *fiber.Ctx) error {
 	schedule := new(model.Schedule)
 
@@ -21,6 +31,15 @@ func CreateSchedule(c *fiber.Ctx) error {
 	return util.CreateResponseMessage(c, util.Success, schedule)
 }
 
+// GetSchedule func gets schedules by given ID or 404 error.
+// @Description Get Schedule by given ID.
+// @Summary get schedule by given ID
+// @Tags Schedules
+// @Accept json
+// @Produce json
+// @Param id path string true "Schedule ID"
+// @Success 200 {object} model.ResponseObject{data=model.Schedule} "desc"
+// @Router /schedules/{id} [get]
 func GetSchedule(c *fiber.Ctx) error {
 	schedule := new(model.Schedule)
 
@@ -30,12 +49,20 @@ func GetSchedule(c *fiber.Ctx) error {
 	return util.CreateResponseMessage(c, util.Success, schedule)
 }
 
+// ListSchedule func gets all existing schedules.
+// @Description Get List of all schedules.
+// @Summary get all existing schedules
+// @Tags Schedules
+// @Accept json
+// @Produce json
+// @Success 200 {object} model.ResponseObject{data=[]model.Schedule} "desc"
+// @Router /schedules [get]
 func ListSchedule(c *fiber.Ctx) error {
 	var schedules []model.Schedule
 	//var d = database.DBConn
 
 	if err := queries.ListSchedule(&schedules); err != nil {
-		return util.CreateResponseMessage(c, util.Success, nil)
+		return util.CreateErrorResponseCode(c, err.Error())
 	}
 
 	return util.CreateResponseMessage(c, model.StatusCode{
@@ -44,6 +71,16 @@ func ListSchedule(c *fiber.Ctx) error {
 	}, schedules)
 }
 
+// UpdateSchedule func updates schedules in the database by checking with id
+// @Description updates schedules in the database by checking with id
+// @Summary update schedule by id
+// @Tags Schedules
+// @Accept json
+// @Produce json
+// @Success 200 {object} model.ResponseObject{data=model.Schedule} "desc"
+// @Param id path string true "Schedule ID"
+// @Param request body model.Schedule true "query params"
+// @Router /schedules/{id} [put]
 func UpdateSchedule(c *fiber.Ctx) error {
 	schedule := new(model.Schedule)
 
@@ -61,6 +98,16 @@ func UpdateSchedule(c *fiber.Ctx) error {
 	}, schedule)
 }
 
+// DeleteEventSchedule func delete schedules in the database by checking with id
+// @Description delete schedules in the database by checking with id
+// @Summary delete schedule by id
+// @Tags Schedules
+// @Accept json
+// @Produce json
+// @Success 200 {object} model.ResponseObject{data=model.Schedule} "desc"
+// @Param id path string true "Schedule ID"
+// @Param request body model.Schedule true "query params"
+// @Router /schedules/{id} [delete]
 func DeleteEventSchedule(c *fiber.Ctx) error {
 	if err := queries.DeleteSchedule(c.Params("id")); err != nil {
 		return util.CreateErrorResponseCode(c, err.Error())
